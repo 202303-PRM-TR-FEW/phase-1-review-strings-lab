9469e1c672ab77210dce84f934eb8f26b8f777d6
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-03-29 11:59:17 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = "Arda Güler";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+console.log(currentUser[0]);
